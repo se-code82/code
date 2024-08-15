e20f32eb3b9798b11b7a43a39cbde70bfbbd57ff
se-code82 <972496183@qq.com>
2024-08-15 08:53:41 +0800

    cross_attn.py

--- main/model/cross_attn.py
+++ main/model/cross_attn.py
@@ -1,5 +1,4 @@
-# From https://github.com/xmu-xiaoma666/External-Attention-pytorch/blob/master/model/attention/CBAM.py with 
-# some modifications
+
 
 import torch
 from torch import nn
@@ -9,47 +8,35 @@ from torch.nn import init
 class CA(nn.Module):
     def __init__(self, channels):
         super(CA, self).__init__()
-
         self.conv7 = nn.Conv2d(in_channels=channels, out_channels=channels, kernel_size=7, padding=3)
         self.conv5 = nn.Conv2d(in_channels=channels, out_channels=channels, kernel_size=5, padding=2)
         self.conv3 = nn.Conv2d(in_channels=channels, out_channels=channels, kernel_size=3, padding=1)
         self.conv1 = nn.Conv2d(in_channels=channels, out_channels=channels, kernel_size=1)
-        # self.relu7 = nn.ReLU(inplace=True)
         self.relu5 = nn.ReLU(inplace=True)
         self.relu3 = nn.ReLU(inplace=True)
         self.relu1 = nn.ReLU(inplace=True)
-
         self.softmax = nn.Softmax(dim=-1)
-
     def forward(self, x):
-
         x7 = self.conv5(x)
         x7 = self.relu5(x7)
-
         x5 = self.conv5(x)
         x5 = self.relu5(x5)
-
         x3 = self.conv3(x)
         x3 = self.relu3(x3)
-
-
         out1 = x3 + x5
         out2 = x7 + x5
         out3 = x3 + x7
         out4 = out1 + out2 + out3
-
         out6 = self.conv1(out4)
         out = out4 * x3
         out = out + x + out6
         out = self.softmax(out)
-
         return out
+        
 class ChannelAttention(nn.Module):
     def __init__(self, channel, reduction):
         super().__init__()
-##################################################################
         self.ca = CA(channels=channel)
-###################################################################
         self.maxpool = nn.AdaptiveMaxPool2d(1)
         self.avgpool = nn.AdaptiveAvgPool2d(1)
         self.se = nn.Sequential(
@@ -59,9 +46,7 @@ class ChannelAttention(nn.Module):
         )
         self.sigmoid = nn.Sigmoid()
     def forward(self, x) :
-##########################################################
         x = self.ca(x)
-############################################################
         max_result = self.maxpool(x)
         avg_result = self.avgpool(x)
         max_out = self.se(max_result)
@@ -81,8 +66,6 @@ class SpatialAttention(nn.Module):
         output = self.conv(result)
         output = self.sigmoid(output)
         return output
-
-
 
 class CBAMBlock(nn.Module):
 
